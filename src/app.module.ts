d4cc9d87f16e20a4d174ec1d95a3af591c7a6086
victorzix <linkgamesplays@gmail.com>
2025-07-23 14:44:38 -0300

    add configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { DatabaseModule } from './providers/database/database.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: (() => {
+        switch (process.env.NODE_ENV) {
+          case 'production':
+            return '.env.production';
+          case 'development':
+          default:
+            return '.env';
+        }
+      })(),
+    }),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_ACCESS_TOKEN_SECRET,
+    }),
+    DatabaseModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,59 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
+import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
+
+const port = process.env.PORT;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    snapshot: true,
+  });
+
+  app.enableCors({ origin: true, credentials: true });
+
+  app.setGlobalPrefix(process.env.APP_ROUTE_PREFIX);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.use(cookieParser());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Vantagens')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup(process.env.SWAGGER_ROUTE_PREFIX, app, document, {
+    swaggerOptions: {
+      operationsSorter: (a, b) => {
+        const methodOrder = { get: 0, post: 1, patch: 2, delete: 3 };
+
+        const methodDiff =
+          methodOrder[a.get('method')] - methodOrder[b.get('method')];
+
+        if (methodDiff !== 0) return methodDiff;
+
+        const pathA = a.get('path');
+        const pathB = b.get('path');
+
+        return pathA.length - pathB.length || pathA.localeCompare(pathB);
+      },
+    },
+  });
+
+  await app.listen(port, () => {
+    Logger.debug(
+      `Servidor rodando em http://localhost:${port}`,
+      'ApplicationServer',
+    );
+
+    Logger.debug(
+      `Documentação rodando em http://localhost:${port}/api/v1/docs`,
+      'Swagger',
+    );
+  });
 }
+
 bootstrap();
