8c1077bae32d45609c5c875ca1d7625b0d40e544
victorzix <linkgamesplays@gmail.com>
2025-07-23 14:43:42 -0300

    add base typeorm config

---
+++ src/providers/database/database.provider.ts
@@ -0,0 +1,22 @@
+import { DataSource } from 'typeorm';
+import { join } from 'path';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST || 'localhost',
+        port: Number(process.env.DATABASE_PORT) || 5432,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        entities: [join(__dirname, '/../../**/*.entity.{ts,js}')],
+        synchronize: true,
+      });
+
+      return dataSource.initialize().then(() => dataSource);
+    },
+  },
+];
